c46eec001077a092b2fb08f56db705455defe396
JesseBuesking <jessebuesking@gmail.com>
2014-11-20 23:30:33 -0800

    modified to support multiple hidden layers

--- dbn/backprop_np.py
+++ dbn/backprop_np.py
@@ -18,109 +18,109 @@ np.random.seed(0)
 # noinspection PyDocstring
 class NN(object):
 
-    def __init__(self, ni, nh, no):
+    def __init__(self, ni, no, h):
         # number of input, hidden, and output nodes
         # +1 for bias node
         self.ni = ni + 1
-        self.nh = nh
         self.no = no
 
+        self.h = h
+        self.nhl = len(h)
+
         # activations for nodes
-        self.ai = np.ones(self.ni)
-        self.ah = np.ones(self.nh)
-        self.ao = np.ones(self.no)
+        self.a = []
+        # for hidden layer(s)
+        for i in range(self.nhl):
+            self.a.append(np.ones(self.h[i]))
+        # for output layer
+        self.a.append(np.ones(self.no))
 
         # create weights
-
-        # uncomment to match the regular backprop.py algorithm
-        # self.wi = makeMatrix(self.ni, self.nh)
-        # self.wo = makeMatrix(self.nh, self.no)
-        # for i in range(self.ni):
-        #     for j in range(self.nh):
-        #         self.wi[i][j] = rand(-0.2, 0.2)
-        # for j in range(self.nh):
-        #     for k in range(self.no):
-        #         self.wo[j][k] = rand(-2.0, 2.0)
-        # self.wi = np.array(self.wi)
-        # self.wo = np.array(self.wo)
-        self.wi = 0.1 * np.random.randn(self.ni, self.nh)
-        self.wo = 0.1 * np.random.randn(self.nh, self.no)
+        self.w = []
+        # for hidden layer(s)
+        for i in range(self.nhl):
+            if i == 0:
+                self.w.append(0.1 * np.random.randn(self.ni, self.h[i]))
+            else:
+                self.w.append(0.1 * np.random.randn(self.h[i-1], self.h[i]))
+        # for output layer
+        self.w.append(0.1 * np.random.randn(self.h[self.nhl-1], self.no))
 
         # last change in weights for momentum
-        self.ci = np.zeros(shape=(self.ni, self.nh))
-        self.co = np.zeros(shape=(self.nh, self.no))
+        self.c = []
+        # for hidden layer(s)
+        for i in range(self.nhl):
+            if i == 0:
+                self.c.append(np.zeros(shape=(self.ni, self.h[i])))
+            else:
+                self.c.append(np.zeros(shape=(self.h[i-1], self.h[i])))
+        # for output layer
+        self.c.append(np.zeros(shape=(self.h[self.nhl-1], self.no)))
 
     def update(self, inputs):
         if len(inputs) != self.ni-1:
             raise ValueError('wrong number of inputs')
 
         # input activations
-        for i in range(self.ni - 1):
-            self.ai[i] = inputs[i]
+        ai = np.array(inputs)
+        ai = np.insert(ai, ai.shape[0], 1)
 
-        # hidden activations
-        s = np.dot(self.ai, self.wi)
-        self.ah = af.func(s)
+        # hidden + output activations
+        s = np.dot(ai, self.w[0])
+        self.a[0] = af.func(s)
+        for i in range(self.nhl):
+            s = np.dot(self.a[i], self.w[i+1])
+            self.a[i + 1] = af.func(s)
 
-        # output activations
-        s = np.dot(self.ah, self.wo)
-        self.ao = af.func(s)
+        return self.a[self.nhl]
 
-        return self.ao
-
-    def backPropagate(self, targets, N, M):
+    def backPropagate(self, inputs, targets, N, M):
         if len(targets) != self.no:
             raise ValueError('wrong number of target values')
 
-        targets = np.array(targets)
+        # input activations
+        ai = np.array(inputs)
+        ai = np.insert(ai, ai.shape[0], 1)
 
+        deltas = []
         # calculate error terms for output
-        error = targets - self.ao
-        output_deltas = af.dfunc(self.ao) * error
+        error = targets - self.a[self.nhl]
+        d = af.dfunc(self.a[self.nhl]) * error
+        deltas.append(d)
 
         # calculate error terms for hidden
-        error = np.dot(output_deltas, self.wo.T)
-        hidden_deltas = af.dfunc(self.ah) * error
+        for i in range(self.nhl, 0, -1):
+            error = np.dot(d, self.w[i].T)
+            d = af.dfunc(self.a[i-1]) * error
+            deltas.append(d)
 
-        # update output weights
-
-        # uncomment for SPEEDUP
-        change = np.outer(output_deltas, self.ah)
-        # change = output_deltas.ravel()[:, np.newaxis] * \
-        #     self.ah.ravel()[np.newaxis, :]
-
-        change = change.T
-        self.wo = self.wo + np.multiply(N, change) + np.multiply(M, self.co)
-        self.co = change
+        # update hidden weights
+        for i in range(self.nhl, 0, -1):
+            change = np.outer(deltas.pop(0), self.a[i - 1])
+            change = change.T
+            self.w[i] = \
+                self.w[i] + \
+                np.multiply(N, change) + \
+                np.multiply(M, self.c[i])
+            self.c[i] = change
 
         # update input weights
-
-        # uncomment for SPEEDUP
-        change = np.outer(hidden_deltas, self.ai)
-        # change = hidden_deltas.ravel()[:, np.newaxis] * \
-        #     self.ai.ravel()[np.newaxis, :]
-
+        change = np.outer(deltas.pop(0), ai)
         change = change.T
-        self.wi = self.wi + np.multiply(N, change) + np.multiply(M, self.ci)
-        self.ci = change
+        self.w[0] = \
+            self.w[0] + \
+            np.multiply(N, change) + \
+            np.multiply(M, self.c[0])
+        self.c[0] = change
 
         # calculate error
         # noinspection PyUnresolvedReferences
-        error = np.multiply(0.5, ((targets-self.ao)**2).sum())
+        error = np.multiply(0.5, ((targets-self.a[self.nhl])**2).sum())
         return error
 
     def test(self, patterns):
         for p in patterns:
             print(p[0], '->', self.update(p[0]))
-
-    def weights(self):
-        print('Input weights:')
-        for i in range(self.ni):
-            print(self.wi[i])
-        print()
-        print('Output weights:')
-        for j in range(self.nh):
-            print(self.wo[j])
 
     def train(self, patterns, iterations=1000, N=0.5, M=0.1):
         # N: learning rate
@@ -133,7 +133,7 @@ class NN(object):
                 inputs = p[0]
                 targets = p[1]
                 self.update(inputs)
-                error = (error + self.backPropagate(targets, N, M))
+                error = (error + self.backPropagate(inputs, targets, N, M))
             if i % print_iter == 0:
                 print('error %-.8f' % error)
 
@@ -149,7 +149,7 @@ def demo():
     ]
 
     # create a network with two input, two hidden, and one output nodes
-    n = NN(2, 2, 1)
+    n = NN(2, 1, [2, 2])
     # train it with some patterns
     start = time.clock()
     n.train(pat, N=0.1, iterations=10000)
